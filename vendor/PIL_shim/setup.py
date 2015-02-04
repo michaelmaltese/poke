ef513ddf21fa6522fb986422d2474242047d020d
Michael Maltese <michaelm@disqus.com>
2015-02-03 18:50:21 -0800

    PIL_shim

---
+++ vendor/PIL_shim/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+install_requires = [
+    'Pillow==2.7.0'
+]
+setup(
+    name='PIL',
+    version='1.1.7',
+    packages=[],
+    install_requires=install_requires
+)
